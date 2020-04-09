efc675882d829671290860558269e595cb07fdb4
Jack Gronenthal <jlg6@illinois.edu>
2020-04-08 23:53:14 -0500

    adding build

---
+++ build/precache-manifest.b1bef81d2d4386318c886220f1a809fa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8727d32889e23a142b0c43f1e9a9ea27",
+    "url": "/crowddit411.web.illinois.edu%20/index.html"
+  },
+  {
+    "revision": "17b54a4157d4486d4c54",
+    "url": "/crowddit411.web.illinois.edu%20/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0fa5ace5b6e6598ba646",
+    "url": "/crowddit411.web.illinois.edu%20/static/css/main.df1bad64.chunk.css"
+  },
+  {
+    "revision": "17b54a4157d4486d4c54",
+    "url": "/crowddit411.web.illinois.edu%20/static/js/2.de0a25d9.chunk.js"
+  },
+  {
+    "revision": "6fea5a45bbb56827f9f82c4c74c29929",
+    "url": "/crowddit411.web.illinois.edu%20/static/js/2.de0a25d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fa5ace5b6e6598ba646",
+    "url": "/crowddit411.web.illinois.edu%20/static/js/main.2d06d397.chunk.js"
+  },
+  {
+    "revision": "340118f2cb18cd166493",
+    "url": "/crowddit411.web.illinois.edu%20/static/js/runtime-main.4cf943c5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/crowddit411.web.illinois.edu%20/static/media/logo.5d5d9eef.svg"
+  }
+]);
